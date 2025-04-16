12666b0ac07025e1ebfdc46d1f10401a73693cd5
Aditya Raj <adityaraj21122018@gmail.com>
2025-04-16 12:46:59 +0530

    LongestConsecutiveSequence

---
+++ Array_Hashing/LongestConsecutiveSequence.java
@@ -0,0 +1,62 @@
+package Array_Hashing;
+
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : set) {
+            // Start a new sequence only if 'num - 1' is not in the set
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = longestConsecutive(nums);
+        System.out.println("Longest consecutive sequence length: " + result);
+    }
+}
+
+
+
+//128. Longest Consecutive Sequence
+
+//Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+//You must write an algorithm that runs in O(n) time.
+
+//Example 1:
+//Input: nums = [100,4,200,1,3,2]
+//Output: 4
+//Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+//Example 2:
+//Input: nums = [0,3,7,2,5,8,4,6,0,1]
+//Output: 9
+//Example 3:
+//Input: nums = [1,0,1,2]
+//Output: 3
+
+
+//Constraints:
+//
+//0 <= nums.length <= 105
+//-109 <= nums[i] <= 109
