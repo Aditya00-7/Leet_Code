126182010340c5e7bd603b73a1bf292376564684
Aditya Raj <adityaraj21122018@gmail.com>
2025-04-20 18:37:21 +0530

    FindMinRotatedSortedArray

---
+++ Binarytrees/FindMinRotatedSortedArray.java
@@ -0,0 +1,31 @@
+package Binarytrees;
+
+public class FindMinRotatedSortedArray {
+
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        if (nums[left] < nums[right]) {
+            return nums[left];
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int result = findMin(nums);
+        System.out.println("Minimum element is: " + result);
+    }
+}
